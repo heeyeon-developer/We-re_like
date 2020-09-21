a743f75eb00ac806f8a7f8022b4068cebde3a9b0
heeyeon_kim <54020487+heeyeon-developer@users.noreply.github.com>
2020-09-21 18:13:55 +0900

    2020.09.21_1.1

--- src/components/Nav/pageNav.js
+++ src/components/Nav/pageNav.js
@@ -18,7 +18,7 @@ class Nav extends Component {
                 <div class='left'>
                     <div style={{float: 'right',display: 'block', width: '15vw', borderColor: '1px solid purple'}}>
                         <a href="#Login" ><p style={{marginTop:'0.3rem'}}>Login</p></a>
-                        <a href="#Select" style={{ marginTop: '0' }}><img style={{ width: '50px' }} src='https://raw.githubusercontent.com/heeyeon-developer/We-re_like/master/public/uploads/take_it.png'></img></a>
+                        <a href="#Select" style={{ marginTop: '0' }}><img src='https://raw.githubusercontent.com/heeyeon-developer/We-re_like/master/public/uploads/take_it.png'></img></a>
                     </div>
                 </div>
             </div>
